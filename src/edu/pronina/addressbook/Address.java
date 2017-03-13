cf404716b84f30b56ed99a7fd43208a122547f10
Kate <ekat.pronina@gmail.com>
2017-03-13 21:25:21 +0300

    Initial commit

---
+++ src/edu/pronina/addressbook/Address.java
@@ -0,0 +1,36 @@
+package edu.pronina.addressbook;
+
+
+public class Address {
+    String street;
+    String house;
+    private String flat;
+
+    Address(String street, String house, String flat) {
+        this.street = street;
+        this.house = house;
+        this.flat = flat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Address address = (Address) o;
+
+        if (!street.equals(address.street)) return false;
+        if (!house.equals(address.house)) return false;
+        return flat.equals(address.flat);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = street.hashCode();
+        result = 31 * result + house.hashCode();
+        result = 31 * result + flat.hashCode();
+        return result;
+    }
+}
+
---
+++ test/edu/pronina/addressbook/AddressBookTest.java
@@ -0,0 +1,38 @@
+package edu.pronina.addressbook;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddressBookTest {
+    @Test
+    public void addContact() throws Exception {
+
+    }
+
+    @Test
+    public void deleteContact() throws Exception {
+
+    }
+
+    @Test
+    public void changeContact() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void peopleSameStreet() throws Exception {
+
+    }
+
+    @Test
+    public void peopleSameHouse() throws Exception {
+
+    }
+
+}
